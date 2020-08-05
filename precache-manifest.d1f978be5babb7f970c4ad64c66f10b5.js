13aa85b088a8f69e3b6c67a5d4272cb41aa65a58
Nickx58 <nikhilx58@gmail.com>
2020-08-05 14:11:25 +0530

    Updates

---
+++ precache-manifest.d1f978be5babb7f970c4ad64c66f10b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0f2cb48152b8f6db93b73978ffa158",
+    "url": "/react-calories-app/index.html"
+  },
+  {
+    "revision": "8cc6f0f252788ba518d4",
+    "url": "/react-calories-app/static/css/main.0d84b4fe.chunk.css"
+  },
+  {
+    "revision": "873d50dcd758fb819a11",
+    "url": "/react-calories-app/static/js/2.c60e0e69.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-calories-app/static/js/2.c60e0e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc6f0f252788ba518d4",
+    "url": "/react-calories-app/static/js/main.bdefbfe3.chunk.js"
+  },
+  {
+    "revision": "0a02d0b39e00ffc7e544",
+    "url": "/react-calories-app/static/js/runtime-main.faa66eb8.js"
+  }
+]);
